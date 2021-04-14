cbcd2bc10c80dad2eb6fa939901b7a1c599daf6d
OyinloluB <oyinloluwab@gmail.com>
2021-04-15 00:47:46 +0100

    custom cursor hover effect in progress

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 
 // components
 import Intro from "../screens/intro/Intro";
@@ -12,6 +12,7 @@ import contactText from "../assets/images/global/contact-text.svg";
 import contactIcon from "../assets/images/global/contact-icon.svg";
 
 const Main = () => {
+  const [cursorSize, setCursorSize] = useState("big");
   const cursor = useRef(null);
 
   const handleCursor = (e) => {
@@ -21,9 +22,16 @@ const Main = () => {
 
   return (
     <>
-      <div className={styles.cursor} ref={cursor}></div>
       <div className={styles.container} onMouseMove={handleCursor}>
-        <div className={styles.sidebar}>
+        <div
+          className={styles.sidebar}
+          onMouseOver={() => {
+            cursor.current.style.transform = "scale(.475)";
+          }}
+          onMouseLeave={() => {
+            cursor.current.style.transform = "scale(1)";
+          }}
+        >
           <img src={logo} alt="plasticbcn-replica-logo" />
           <div>
             <img src={contactText} alt="plasticbcn-replica-contactText" />
@@ -31,10 +39,19 @@ const Main = () => {
           </div>
         </div>
         <Intro />
-        <div className={styles.hamburger}>
+        <div
+          className={styles.hamburger}
+          onMouseOver={() => {
+            cursor.current.style.transform = "scale(.475)";
+          }}
+          onMouseLeave={() => {
+            cursor.current.style.transform = "scale(1)";
+          }}
+        >
           <div></div>
           <div></div>
         </div>
+        <div className={styles.cursor} ref={cursor}></div>
       </div>
     </>
   );
