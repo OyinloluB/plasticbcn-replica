fb7de7ad0bb158f742bc64ac4b4df7673af39335
OyinloluB <oyinloluwab@gmail.com>
2021-05-01 23:22:56 +0100

    intro page clean up

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useEffect } from "react";
 
 // components
 import Intro from "../screens/intro/Intro";
@@ -13,17 +13,53 @@ import contactIcon from "../assets/images/global/contact-icon.svg";
 
 const Main = () => {
   const cursor = useRef(null);
+  const logoHover = useRef(null);
+  let options = {
+    distance: 100,
+  };
+  let width;
+  let height;
+  let diagonal;
+  let maxDistance;
 
   const handleCursor = (e) => {
     cursor.current.style.left = e.pageX + "px";
     cursor.current.style.top = e.pageY + "px";
   };
 
+  useEffect(() => {
+    width = logoHover.offsetWidth;
+    height = logoHover.offsetHeight;
+    diagonal = Math.sqrt(Math.pow(width, 2) + Math.pow(height, 2));
+    maxDistance = (diagonal + options.distance) / 2;
+    logoHover.style.transition = "0.6s cubic-bezier(0.075, 0.82, 0.165, 1)";
+  }, []);
+
+  const handleMagnet = () => {
+    if (options.distance < maxDistance) {
+      let percent = 1 - options.distance / maxDistance;
+      logoHover.style.transform = `
+            translate3d(${Math.round(x * percent)}px, ${Math.round(
+        y * percent
+      )}px, 0) 
+            rotateY(${Math.round(x * percent)}deg) 
+            rotateX(${Math.round(y * percent)}deg)
+          `;
+    } else {
+      logoHover.style.transform = "";
+    }
+  };
+
   return (
     <>
       <div className={styles.container} onMouseMove={handleCursor}>
         <div className={styles.sidebar}>
-          <img src={logo} alt="plasticbcn-replica-logo" />
+          <img
+            src={logo}
+            ref={logoHover}
+            alt="plasticbcn-replica-logo"
+            onMouseMove={handleMagnet}
+          />
           <div>
             <img src={contactText} alt="plasticbcn-replica-contactText" />
             <img src={contactIcon} alt="plasticbcn-replica-contactIcon" />
--- src/screens/intro/Intro.js
+++ src/screens/intro/Intro.js
@@ -7,9 +7,28 @@ const Intro = () => {
     <div className={styles.container}>
       <div>
         <h2>Plastic is an award-winning international agency.</h2>
-        <h1>
-          <span className={styles.fixed}>We create remarkable</span>
-          <span className={styles.animated}></span>
+        <h1 className={styles.fixed}>We create remarkable</h1>
+        <h1 className={styles.animatedText}>
+          <span className={styles.animatedText_firstword}>
+            <span className={styles.animatedText_word1}>d</span>
+            <span className={styles.animatedText_word2}>i</span>
+            <span className={styles.animatedText_word3}>g</span>
+            <span className={styles.animatedText_word4}>i</span>
+            <span className={styles.animatedText_word5}>t</span>
+            <span className={styles.animatedText_word6}>a</span>
+            <span className={styles.animatedText_word7}>l</span>
+          </span>
+          <span className={styles.animatedText_secondword}>
+            <span className={styles.animatedText_word8}>p</span>
+            <span className={styles.animatedText_word9}>r</span>
+            <span className={styles.animatedText_word10}>o</span>
+            <span className={styles.animatedText_word11}>d</span>
+            <span className={styles.animatedText_word12}>u</span>
+            <span className={styles.animatedText_word13}>c</span>
+            <span className={styles.animatedText_word14}>t</span>
+            <span className={styles.animatedText_word15}>s</span>
+            <span className={styles.animatedText_word16}>.</span>
+          </span>
         </h1>
         <button className={styles.cta}>Our showreel</button>
       </div>
