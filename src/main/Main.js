90d2bd6bb48327f958935ade3745557d391d13b0
OyinloluB <oyinloluwab@gmail.com>
2021-05-04 00:27:43 +0100

    magnetic effect on hover

--- src/main/Main.js
+++ src/main/Main.js
@@ -13,52 +13,59 @@ import contactIcon from "../assets/images/global/contact-icon.svg";
 
 const Main = () => {
   const cursor = useRef(null);
-  const logoHover = useRef(null);
+  const plasticLogo = useRef(null);
+  const contact = useRef(null);
+  const menu = useRef(null);
 
   const handleCursor = (e) => {
     cursor.current.style.left = e.pageX + "px";
     cursor.current.style.top = e.pageY + "px";
   };
 
-  const handleMagnet = (e) => {
-    let x = logoHover.current.offsetX;
-    let y = logoHover.current.offsetY;
-    let width = logoHover.current.offsetWidth;
-    let height = logoHover.current.offsetHeight;
+  const handleMagnet = (e, item) => {
+    let x = e.nativeEvent.offsetX;
+    let y = e.nativeEvent.offsetY;
+    let width = item.current.offsetWidth;
+    let height = item.current.offsetHeight;
     let move = 30;
     let xMove = (x / width) * (move * 2) - move;
     let yMove = (y / height) * (move * 2) - move;
 
-    logoHover.current.style.transform = `translate(${xMove}px, ${yMove}px)`;
-
-    console.log(x, y, width, height, xMove, yMove);
-
-    // if (e.type === "mouseleave") logoHover.current.style.transform = "";
+    item.current.style.transform = `translate(${xMove}px, ${yMove}px)`;
   };
 
-  const stopMagnet = (e) => {
-    logoHover.current.style.transform = "";
+  const stopMagnet = (item) => {
+    item.current.style.transform = "";
   };
 
   return (
     <>
       <div className={styles.container} onMouseMove={handleCursor}>
         <div className={styles.sidebar}>
-          <img
-            src={logo}
-            ref={logoHover}
-            alt="plasticbcn-replica-logo"
-            onMouseMove={handleMagnet}
-            onMouseLeave={stopMagnet}
-          />
-          <div>
+          <div
+            ref={plasticLogo}
+            onMouseMove={(e) => handleMagnet(e, plasticLogo)}
+            onMouseLeave={() => stopMagnet(plasticLogo)}
+          >
+            <img src={logo} alt="plasticbcn-replica-logo" />
+          </div>
+          <div
+            ref={contact}
+            onMouseMove={(e) => handleMagnet(e, contact)}
+            onMouseLeave={() => stopMagnet(contact)}
+          >
             <img src={contactText} alt="plasticbcn-replica-contactText" />
             <img src={contactIcon} alt="plasticbcn-replica-contactIcon" />
           </div>
         </div>
         <Intro />
         <div className={styles.hamburger}>
-          <div className={styles.wrapper}>
+          <div
+            className={styles.wrapper}
+            ref={menu}
+            onMouseMove={(e) => handleMagnet(e, menu)}
+            onMouseLeave={() => stopMagnet(menu)}
+          >
             <div className={styles.menu}></div>
             <div className={styles.menu}></div>
           </div>
