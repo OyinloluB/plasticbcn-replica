a2ffa86bf411d3030f08a7375d89497cdb97c56d
OyinloluB <oyinloluwab@gmail.com>
2021-05-10 13:43:05 +0100

    slide down nav

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState } from "react";
 
 // components
 import Intro from "../screens/intro/Intro";
@@ -12,12 +12,19 @@ import contactText from "../assets/images/global/contact-text.svg";
 import contactIcon from "../assets/images/global/contact-icon.svg";
 
 const Main = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const cursor = useRef(null);
   const plasticLogo = useRef(null);
   const contact = useRef(null);
   const menu = useRef(null);
+  const menuWrapper = useRef(null);
+  const topMenuLine = useRef(null);
+  const bottomMenuLine = useRef(null);
+  const overlay = useRef(null);
 
   const handleCursor = (e) => {
+    console.log((cursor.current.style.left = e.pageX + "px"));
     cursor.current.style.left = e.pageX + "px";
     cursor.current.style.top = e.pageY + "px";
   };
@@ -38,10 +45,33 @@ const Main = () => {
     item.current.style.transform = "";
   };
 
+  const openOverlay = () => {
+    if (menuOpen === true) {
+      overlay.current.style.height = "100%";
+      menuWrapper.current.style.zIndex = "1";
+      topMenuLine.current.style.transform = "translateY(3.5px) rotate(45deg)";
+      topMenuLine.current.style.background = "#ffffff";
+      bottomMenuLine.current.style.transform =
+        "translateX(0) translateY(-3.5px) rotate(-45deg)";
+      bottomMenuLine.current.style.background = "#ffffff";
+      bottomMenuLine.current.style.width = "22px";
+    } else {
+      overlay.current.style.height = "0%";
+      menuWrapper.current.style.zIndex = "unset";
+      topMenuLine.current.style.transform = "unset";
+      topMenuLine.current.style.background = "#000000";
+      bottomMenuLine.current.style.transform = "unset";
+      bottomMenuLine.current.style.background = "#000000";
+      bottomMenuLine.current.style.width = "16px";
+    }
+
+    setMenuOpen(!menuOpen);
+  };
+
   return (
-    <>
-      <div className={styles.navContent}></div>
-      <div className={styles.container} onMouseMove={handleCursor}>
+    <div className={styles.container} onMouseMove={handleCursor}>
+      <div className={styles.navContent} ref={overlay}></div>
+      <div className={styles.container_wrapper}>
         <div className={styles.sidebar}>
           <div
             ref={plasticLogo}
@@ -60,22 +90,23 @@ const Main = () => {
           </div>
         </div>
         <Intro />
-        <div className={styles.hamburger}>
+        <div className={styles.hamburger} ref={menuWrapper}>
           <div
             className={styles.wrapper}
             ref={menu}
             onMouseMove={(e) => handleMagnet(e, menu)}
             onMouseLeave={() => stopMagnet(menu)}
+            onClick={openOverlay}
           >
-            <div className={styles.menu}></div>
-            <div className={styles.menu}></div>
+            <div className={styles.menu} ref={topMenuLine}></div>
+            <div className={styles.menu} ref={bottomMenuLine}></div>
           </div>
           <div className={styles.cursor} ref={cursor}>
             <span>Scroll</span>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
